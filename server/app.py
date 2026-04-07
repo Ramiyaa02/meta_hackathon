19c3434838e8730d31a0954264cb963c5a74b0bc
S-Ramiyaa <Ramiyaa@users.noreply.huggingface.co>
2026-04-07 17:47:00 +0000

    Update server/app.py

--- server/app.py
+++ server/app.py
@@ -3,3 +3,10 @@ import sys
 from pathlib import Path
 sys.path.insert(0, str(Path(__file__).parent.parent))
 from app import app
+import uvicorn
+
+def main():
+    uvicorn.run(app, host="0.0.0.0", port=7860)
+
+if __name__ == "__main__":
+    main()
